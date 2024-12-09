cf766e9ac68a6c1ea1ec56924dfc7635a4aa9656
karthik <karthikeyan14june@gmail.com>
2024-12-09 14:07:57 +0530

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,1 @@
-git log v1.0..v2.0 --merges --pretty=format:"%B" | while read -r commit_message; do
-  # Assuming user-written title is the first line of commit message
-  user_title=$(echo "$commit_message" | head -n 1)
-  echo "User-written Title: $user_title"
-done
+NOTES=$(git log ${{ github.event.before }}..${{ github.ref }} --merges --pretty=format:"- %s by %an in pull request %H")
