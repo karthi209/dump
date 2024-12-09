a6a9707fe570165a90e9f69e0bd34e3a4057c2cd
karthik <karthikeyan14june@gmail.com>
2024-12-09 11:04:38 +0530

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 for sha in $(git log v1.0..v2.0 --merges --pretty=format:"%H"); do
-  gh pr view --repo OWNER/REPO --json number,title,mergedAt --commit $sha | jq -r '"#\(.number): \(.title) (merged on \(.mergedAt))"'
+  gh pr list --state merged --search "$sha" --json number,title,mergedAt --repo OWNER/REPO | \
+  jq -r '"#\(.number): \(.title) (merged on \(.mergedAt))"'
 done
